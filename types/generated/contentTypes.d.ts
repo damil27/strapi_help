41ffe5c514a7638d5176055070d4186df9887702
damin <34189426+damil27@users.noreply.github.com>
2025-05-02 20:29:13 +0100

    add new field to author collection

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -469,6 +469,7 @@ export interface ApiAuthorAuthor extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    website: Schema.Attribute.String;
   };
 }
 
