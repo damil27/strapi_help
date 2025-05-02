7dde316b36e8c13cdc997e17025039105a87adc2
damin <34189426+damil27@users.noreply.github.com>
2025-05-02 20:05:52 +0100

    Modication to the form

--- config/database.js
+++ config/database.js
@@ -60,7 +60,7 @@ module.exports = ({ env }) => {
         filename: path.join(
           __dirname,
           "..",
-          env("DATABASE_FILENAME", "data.db")
+          env("DATABASE_FILENAME", "dev_data.db")
         ),
       },
       useNullAsDefault: true,
